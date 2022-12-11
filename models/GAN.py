1cf5fd48e86d74a05f05a889275355bb855427cc
1412SamZ <samuelzhang0913@gmail.com>
2022-12-11 22:34:42 +0100

    g

--- models/GAN.py
+++ models/GAN.py
@@ -29,23 +29,23 @@ class Discriminator(nn.Module):
 class Generator(nn.Module):
     def __init__(self, latent_dim):
         super().__init__()
-        self.lin1 = nn.Linear(latent_dim, 7*7*64)  # [n, 256, 7, 7]
-        self.ct1 = nn.ConvTranspose2d(64, 32, 4, stride=2) # [n, 64, 16, 16]
-        self.ct2 = nn.ConvTranspose2d(32, 16, 4, stride=2) # [n, 16, 34, 34]
-        self.conv = nn.Conv2d(16, 1, kernel_size=7)  # [n, 1, 28, 28]
+        self.lin1 = nn.Linear(latent_dim, 7*7*64)  
+        self.ct1 = nn.ConvTranspose2d(64, 32, 4, stride=2) 
+        self.ct2 = nn.ConvTranspose2d(32, 16, 4, stride=2) 
+        self.conv = nn.Conv2d(16, 1, kernel_size=7)  
     
 
     def forward(self, x):
         # Pass latent space input into linear layer and reshape
         x = self.lin1(x)
         x = F.relu(x)
-        x = x.view(-1, 64, 7, 7)  #256
+        x = x.view(-1, 64, 7, 7)  
         
-        # Upsample (transposed conv) 16x16 (64 feature maps)
+        # Upsample (transposed conv) 
         x = self.ct1(x)
         x = F.relu(x)
         
-        # Upsample to 34x34 (16 feature maps)
+        # Upsample 
         x = self.ct2(x)
         x = F.relu(x)
         
